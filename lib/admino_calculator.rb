9b27a054e05487fc22307ad571520ece3eb2bc60
Ashrut Panchal <ashrutpanchal112@gmail.comcom>
2025-09-08 13:09:25 +0530

    Alternatively we can use the commented code

--- lib/admino_calculator.rb
+++ lib/admino_calculator.rb
@@ -34,3 +34,27 @@ begin
 rescue => e
   puts e.message
 end
+
+
+# Alternatively we use the below code to generate the string calculator 
+
+
+# class StringCalculator
+#   def self.add(numbers)
+#     return 0 if numbers.empty?
+#     if numbers.start_with?("//")
+#       delimiter = numbers[2]
+#       numbers = numbers[4..]
+#     else
+#       delimiter = /,|\n/
+#     end
+
+#     num_array = numbers.split(delimiter).map(&:to_i)
+#     negatives = num_array.select { |n| n < 0 }
+#     unless negatives.empty?
+#       raise "negative numbers not allowed: #{negatives.join(', ')}"
+#     end
+
+#     num_array.sum
+#   end
+# end
